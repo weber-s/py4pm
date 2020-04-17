ad29b04c4595e71254280dd47d6c17055ac1e397
webersa <samuel.weber@univ-grenoble-alpes.fr>
2020-04-17 15:21:09 +0200

    add requirement and update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,29 @@ PACKAGES = [
 
 setup(
     name='py4pm',
-    version='0.3',
-    packages=PACKAGES
+    version='0.5',
+    packages=PACKAGES,
+    install_requires=['pandas', 'xlrd', 'matplotlib'],
+    python_requires='>=3',
+    author='Samuël Weber',
+    author_email='samuel.weber@univ-grenoble-alpes.fr',
+    project_urls={
+        'Documentation': 'https://py4pm.readthedocs.io/en/latest/',
+        'Source': 'https://gricad-gitlab.univ-grenoble-alpes.fr/webersa/py4pm',
+	},
+    classifiers=[
+	# How mature is this project? Common values are
+	#   3 - Alpha
+	#   4 - Beta
+	#   5 - Production/Stable
+	'Development Status :: 3 - Alpha',
+
+	# Indicate who your project is intended for
+	'Intended Audience :: Scientist',
+	'Topic :: Atmospheric Science :: PMF',
+
+	# Specify the Python versions you support here. In particular, ensure
+	# that you indicate whether you support Python 2, Python 3 or both.
+	'Programming Language :: Python :: 3',
+	],
 )
