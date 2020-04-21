3314966e4d717b6f069768153c999feead3817f2
webersa <samuel.weber@univ-grenoble-alpes.fr>
2020-04-21 14:05:30 +0200

    update setup to have the READMER

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
+from os import path
+
+VERSION = '0.6.1'
 
 PACKAGES = [
     'py4pm',
 ]
 
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='py4pm',
-    version='0.6',
+    version=VERSION,
     packages=PACKAGES,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://gricad-gitlab.univ-grenoble-alpes.fr/webersa/py4pm',
     install_requires=['pandas', 'xlrd', 'matplotlib'],
     python_requires='>=3',
