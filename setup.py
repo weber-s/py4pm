8b2d5e3ebbfe11aaf70f1dc9544fa40529ea2424
webersa <samuel.weber@univ-grenoble-alpes.fr>
2020-07-30 11:31:42 +0200

    try to add README

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 from os import path
 
-VERSION = '0.6.2'
+VERSION = '0.6.3'
 
 PACKAGES = [
-    'py4pm',
-]
+        'py4pm',
+        ]
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
+    long_description = f.read()
 
 setup(
     name='py4pm',
     version=VERSION,
     packages=PACKAGES,
+    include_package_data=True,    # include everything in source control
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://gricad-gitlab.univ-grenoble-alpes.fr/webersa/py4pm',
@@ -26,20 +27,20 @@ setup(
     project_urls={
         'Documentation': 'https://py4pm.readthedocs.io/en/latest/',
         'Source': 'https://gricad-gitlab.univ-grenoble-alpes.fr/webersa/py4pm',
-	},
+        },
     classifiers=[
-	# How mature is this project? Common values are
-	#   3 - Alpha
-	#   4 - Beta
-	#   5 - Production/Stable
-	'Development Status :: 3 - Alpha',
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-	# Indicate who your project is intended for
+        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
 
-	# Specify the Python versions you support here. In particular, ensure
-	# that you indicate whether you support Python 2, Python 3 or both.
-	'Programming Language :: Python :: 3',
-	],
-)
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        ],
+    )
